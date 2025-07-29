034aeb2d1c9a4693439f743609474f303ed7c5b0
jayapriya45 <j64323768@gmail.com>
2025-07-29 10:48:03 +0530

    py2

---
+++ py2.py
@@ -0,0 +1,14 @@
+print("TAKSHASHILA UNIVERSITY")
+print("Ongur,tindivanam,villupuram dist,tn")
+print("--------------------")
+print("Student mark list")
+print("---------------")
+str1=input("Enter the register number:")
+str2=input("Enter the name:")
+m1=int(input("Python mark:"))
+m2=int(input("DBMS mark:"))
+m3=int(input("Account mark:"))
+total=m1+m2+m3
+print("total mark:",total)
+average=total/3
+print("Average mark:",average)
