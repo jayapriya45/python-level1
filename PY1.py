fc3f8e6d7723a46bb8db22380b25d1225db507dc
jayapriya45 <j64323768@gmail.com>
2025-07-29 10:38:46 +0530

    program 1

---
+++ PY1.py
@@ -0,0 +1,16 @@
+print("ARITHMETIC OPERATION")
+print("--------------------")
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("Result")
+c=a+b
+print("Add:",c)
+c=a-b
+print("Sub:",c)
+c=a*b
+print("Multi:",c)
+c=a/b
+print("Div:",c)
+c=a%b
+print("Mod:",c )
+      
